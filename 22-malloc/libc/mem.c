da03309faa4c5d87aa7be63a5ba541e96ea216dc
Harvey Xing <71458491+xing1357@users.noreply.github.com>
2023-08-16 09:28:43 -0500

    Fix kmalloc, add some wrapper functions.

--- 22-malloc/libc/mem.c
+++ 22-malloc/libc/mem.c
@@ -18,9 +18,9 @@ void memory_set(u8 *dest, u8 val, u32 len) {
 u32 free_mem_addr = 0x10000;
 /* Implementation is just a pointer to some free memory which
  * keeps growing */
-u32 kmalloc(u32 size, int align, u32 *phys_addr) {
+u32 kmalloc_int(u32 size, int align, u32 *phys_addr) {
     /* Pages are aligned to 4K, or 0x1000 */
-    if (align == 1 && (free_mem_addr & 0xFFFFF000)) {
+    if (align == 1 && (free_mem_addr & 0x00000FFF)) { // Check if the address is not already page aligned
         free_mem_addr &= 0xFFFFF000;
         free_mem_addr += 0x1000;
     }
@@ -30,4 +30,12 @@ u32 kmalloc(u32 size, int align, u32 *phys_addr) {
     u32 ret = free_mem_addr;
     free_mem_addr += size; /* Remember to increment the pointer */
     return ret;
+}
+
+u32 kmalloc_aligned(u32 size){
+    kmalloc_int(size, 1, 0);
+}
+
+u32 kmalloc(u32 size){
+    kmalloc_int(size, 0, 0);
 }
